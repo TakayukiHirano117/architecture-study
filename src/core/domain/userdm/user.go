99f89404a7e56fb0ff9c203b4377be66df7c90d5
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-07-23 20:51:48 +0900

    コンフリクト修正、この切り方はもうしない

--- src/core/domain/userdm/user.go
+++ src/core/domain/userdm/user.go
@@ -7,47 +7,30 @@ import (
 )
 
 type User struct {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  id UserID
-=======
-  id UserId
->>>>>>> d99dc49 (fix conflicts)
-  name string
-  password Password
-  
-=======
 	id               UserId
 	name             UserName
 	password         Password
 	skills           []Skill
 	careers          []*Career
 	email            Email
-<<<<<<< HEAD
 	selfIntroduction SelfIntroduction
-<<<<<<< HEAD
->>>>>>> dfd255d (バリューオブジェクト実装途中、一旦コミット)
-=======
-=======
-	selfIntroduction *SelfIntroduction
->>>>>>> 6b0b5cf (エンティティと値オブジェクトの実装)
 	createdAt        time.Time
 	updatedAt        time.Time
->>>>>>> 68d42e9 (値オブジェクトやエンティティなど実装、一旦コミット)
 }
 
-func NewUser(name UserName, password Password, skills []Skill, careers []*Career, email Email, selfIntroduction *SelfIntroduction) (*User, error) {
+func NewUser(id UserId, name UserName, password Password, skills []Skill, careers []*Career, email Email, selfIntroduction *SelfIntroduction) (*User, error) {
 	if len(skills) <= 0 {
 		return nil, errors.New("skills must be at least 1")
 	}
 
 	return &User{
+		id:               id,
 		name:             name,
 		password:         password,
-		email:            email,
-		selfIntroduction: selfIntroduction,
 		skills:           skills,
 		careers:          careers,
+		email:            email,
+		selfIntroduction: *selfIntroduction,
 		createdAt:        time.Now(),
 		updatedAt:        time.Now(),
 	}, nil
