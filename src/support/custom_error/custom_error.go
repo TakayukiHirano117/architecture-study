60911b61926b12a166a174220cb51b5050c3d916
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-08-19 14:04:26 +0900

    404のエラー定義追加

--- src/support/custom_error/custom_error.go
+++ src/support/custom_error/custom_error.go
@@ -36,11 +36,19 @@ func (e *appErr) Trace() error {
 	return e.trace
 }
 
+
+// TODO: エラーの種類に応じてメソッドとtypeを追加する 405
+// 401, 403はログインを実装したら追加する
+
 type BadRequestErr struct {
 	*appErr
 }
 
 type InternalErr struct {
+	*appErr
+}
+
+type NotFoundErr struct {
 	*appErr
 }
 
@@ -53,10 +61,30 @@ func BadRequest(msg string) *BadRequestErr {
 		},
 	}
 }
-// TODO: BadRequestの詳細度に応じてメソッドを追加する
-// TODO: エラーの種類に応じてメソッドとtypeを追加する 404, 405
-// 401, 403はログインを実装したら追加する
 
+func BadRequestf(format string, msg ...any) *BadRequestErr {
+	message := fmt.Sprintf(format, msg...)
+
+	return &BadRequestErr{
+		&appErr{
+			code:  http.StatusBadRequest,
+			msg:   message,
+			trace: errors.Errorf(format, msg...),
+		},
+	}
+}
+
+func BadRequestWrapf(err2 error, format string, msg ...any) *BadRequestErr {
+	message := fmt.Sprintf(format, msg...)
+
+	return &BadRequestErr{
+		&appErr{
+			code:  http.StatusBadRequest,
+			msg:   message,
+			trace: errors.Wrapf(err2, format, msg...),
+		},
+	}
+}
 
 func Internal(msg string) *InternalErr {
 	return &InternalErr{
@@ -86,6 +114,40 @@ func InternalWrapf(err2 error, format string, msg ...any) *InternalErr {
 	return &InternalErr{
 		&appErr{
 			code:  http.StatusInternalServerError,
+			msg:   message,
+			trace: errors.Wrapf(err2, format, msg...),
+		},
+	}
+}
+
+func NotFound(msg string) *NotFoundErr {
+	return &NotFoundErr{
+		&appErr{
+			code:  http.StatusNotFound,
+			msg:   msg,
+			trace: errors.New(msg),
+		},
+	}
+}
+
+func NotFoundf(format string, msg ...any) *NotFoundErr {
+	message := fmt.Sprintf(format, msg...)
+
+	return &NotFoundErr{
+		&appErr{
+			code:  http.StatusNotFound,
+			msg:   message,
+			trace: errors.Errorf(format, msg...),
+		},
+	}
+}
+
+func NotFoundWrapf(err2 error, format string, msg ...any) *NotFoundErr {
+	message := fmt.Sprintf(format, msg...)
+
+	return &NotFoundErr{
+		&appErr{
+			code:  http.StatusNotFound,
 			msg:   message,
 			trace: errors.Wrapf(err2, format, msg...),
 		},
