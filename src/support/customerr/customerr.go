962964c27a3643e0b7d5c20f4b6d2aebc9dc788e
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-08-20 21:05:58 +0900

    401, 403, 409の独自エラー定義

--- src/support/customerr/customerr.go
+++ src/support/customerr/customerr.go
@@ -36,15 +36,18 @@ func (e *appErr) Trace() error {
 	return e.trace
 }
 
-
-// TODO: エラーの種類に応じてメソッドとtypeを追加する 405
+// TODO: エラーの種類に応じてメソッドとtypeを追加する
 // 401, 403はログインを実装したら追加する
 
 type BadRequestErr struct {
 	*appErr
 }
 
-type InternalErr struct {
+type UnauthorizedErr struct {
+	*appErr
+}
+
+type ForbiddenErr struct {
 	*appErr
 }
 
@@ -52,6 +55,16 @@ type NotFoundErr struct {
 	*appErr
 }
 
+type ConflictErr struct {
+	*appErr
+}
+
+type InternalErr struct {
+	*appErr
+}
+
+// 400番台
+// 400
 func BadRequest(msg string) *BadRequestErr {
 	return &BadRequestErr{
 		&appErr{
@@ -86,40 +99,76 @@ func BadRequestWrapf(err2 error, format string, msg ...any) *BadRequestErr {
 	}
 }
 
-func Internal(msg string) *InternalErr {
-	return &InternalErr{
+// 401
+func Unauthorized(msg string) *UnauthorizedErr {
+	return &UnauthorizedErr{
 		&appErr{
-			code:  http.StatusInternalServerError,
+			code:  http.StatusUnauthorized,
 			msg:   msg,
 			trace: errors.New(msg),
 		},
 	}
 }
 
-func Internalf(format string, msg ...any) *InternalErr {
+func Unauthorizedf(format string, msg ...any) *UnauthorizedErr {
 	message := fmt.Sprintf(format, msg...)
 
-	return &InternalErr{
+	return &UnauthorizedErr{
 		&appErr{
-			code:  http.StatusInternalServerError,
+			code:  http.StatusUnauthorized,
 			msg:   message,
 			trace: errors.Errorf(format, msg...),
 		},
 	}
 }
 
-func InternalWrapf(err2 error, format string, msg ...any) *InternalErr {
+func UnauthorizedWrapf(err2 error, format string, msg ...any) *UnauthorizedErr {
 	message := fmt.Sprintf(format, msg...)
 
-	return &InternalErr{
+	return &UnauthorizedErr{
 		&appErr{
-			code:  http.StatusInternalServerError,
+			code:  http.StatusUnauthorized,
 			msg:   message,
 			trace: errors.Wrapf(err2, format, msg...),
 		},
 	}
 }
 
+// 403
+
+func Forbidden(msg string) *ForbiddenErr {
+	return &ForbiddenErr{
+		&appErr{
+			code:  http.StatusForbidden,
+			msg:   msg,
+			trace: errors.New(msg),
+		},
+	}
+}
+
+func Forbiddenf(format string, msg ...any) *ForbiddenErr {
+	message := fmt.Sprintf(format, msg...)
+	return &ForbiddenErr{
+		&appErr{
+			code:  http.StatusForbidden,
+			msg:   message,
+			trace: errors.Errorf(format, msg...),
+		},
+	}
+}
+
+func ForbiddenWrapf(err2 error, format string, msg ...any) *ForbiddenErr {
+	message := fmt.Sprintf(format, msg...)
+	return &ForbiddenErr{
+		&appErr{
+			code:  http.StatusForbidden,
+			msg:   message,
+			trace: errors.Wrapf(err2, format, msg...),
+		},
+	}
+}
+
+// 404
 func NotFound(msg string) *NotFoundErr {
 	return &NotFoundErr{
 		&appErr{
@@ -148,6 +197,76 @@ func NotFoundWrapf(err2 error, format string, msg ...any) *NotFoundErr {
 	return &NotFoundErr{
 		&appErr{
 			code:  http.StatusNotFound,
+			msg:   message,
+			trace: errors.Wrapf(err2, format, msg...),
+		},
+	}
+}
+
+// 409
+func Conflict(msg string) *ConflictErr {
+	return &ConflictErr{
+		&appErr{
+			code:  http.StatusConflict,
+			msg:   msg,
+			trace: errors.New(msg),
+		},
+	}
+}
+
+func Conflictf(format string, msg ...any) *ConflictErr {
+	message := fmt.Sprintf(format, msg...)
+
+	return &ConflictErr{
+		&appErr{
+			code:  http.StatusConflict,
+			msg:   message,
+			trace: errors.Errorf(format, msg...),
+		},
+	}
+}
+
+func ConflictWrapf(err2 error, format string, msg ...any) *ConflictErr {
+	message := fmt.Sprintf(format, msg...)
+
+	return &ConflictErr{
+		&appErr{
+			code:  http.StatusConflict,
+			msg:   message,
+			trace: errors.Wrapf(err2, format, msg...),
+		},
+	}
+}
+
+// 500番台
+func Internal(msg string) *InternalErr {
+	return &InternalErr{
+		&appErr{
+			code:  http.StatusInternalServerError,
+			msg:   msg,
+			trace: errors.New(msg),
+		},
+	}
+}
+
+func Internalf(format string, msg ...any) *InternalErr {
+	message := fmt.Sprintf(format, msg...)
+
+	return &InternalErr{
+		&appErr{
+			code:  http.StatusInternalServerError,
+			msg:   message,
+			trace: errors.Errorf(format, msg...),
+		},
+	}
+}
+
+func InternalWrapf(err2 error, format string, msg ...any) *InternalErr {
+	message := fmt.Sprintf(format, msg...)
+
+	return &InternalErr{
+		&appErr{
+			code:  http.StatusInternalServerError,
 			msg:   message,
 			trace: errors.Wrapf(err2, format, msg...),
 		},
